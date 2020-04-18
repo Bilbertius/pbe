69d07a75c368d96bcaf59fad855b83fe4cd635ba
Bilbertius <lauchlinmacneil@gmail.com>
2020-04-18 00:30:08 -0600

    bleh

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const { PORT } = require('./config');
 
 const app = express();
 
-app.use(cors({ origin: 'http://petfulest.now.sh'}));
+app.use(cors({ origin: 'http://petful-pearl.now.sh'}));
 
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganSetting));
